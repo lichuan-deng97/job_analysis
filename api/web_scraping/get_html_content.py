ef0823a3138cd36f60a17e514269697505ee6789
lichuan-deng97 <80370626+lichuan-deng97@users.noreply.github.com>
2021-03-30 13:00:50 -0400

    Create get_html_content.py

---
+++ api/web_scraping/get_html_content.py
@@ -0,0 +1,7 @@
+import requests
+
+def get_url_content(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    html = response.text
+    return html
